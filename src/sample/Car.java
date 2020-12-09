e8950014cd89116901b0f8a92b57e8b3bc5a460e
Pinioo <jakpin1999@gmail.com>
2020-12-09 15:13:42 +0100

    Completed

--- src/sample/Car.java
+++ src/sample/Car.java
@@ -3,35 +3,32 @@ package sample;
 import net.sourceforge.jFuzzyLogic.rule.FuzzyRuleSet;
 
 public class Car {
+    private final double updatesPerSec = 1000.0/Main.frameDelay;
+    private boolean brake = false;
     public int gear = 1;
-    public double speed = 0;
+    public double speed = 5;
     public double throttle = 50;
+    public double roadTilt = 0;
     private final FuzzyRuleSet fuzzyRules;
 
-    private final double[] ratio = {210, 70, 50, 35, 28, 21};
-
-    public void gearTest() {
-        double[] speeds = {10, 30, 50, 70, 90, 120};
-        for(int i = 0; i < 6; i++) {
-            System.out.println((i+1) + ", " + speeds[i] + " km/h, " + speeds[i]*ratio[i] + " rpm");
-        }
-    }
+    private final double[] ratio = {190, 90, 50, 35, 28, 21};
 
     public Car(FuzzyRuleSet fuzzyRules){
         this.fuzzyRules = fuzzyRules;
     }
 
     public void setGear(double gear){
-        if(!(this.gear - 0.7 < gear && gear < this.gear + 0.7))
+        if(!(this.gear - 0.55 < gear && gear < this.gear + 0.55))
             this.gear = Math.toIntExact(Math.round(gear));
     }
 
     public void update(){
-        speed += calculateAcceleration();
+        speed += calculateSpeedUpdate();
+        speed = Math.max(0, speed);
 
         fuzzyRules.setVariable("speed", this.speed);
         fuzzyRules.setVariable("throttle", this.throttle);
-        fuzzyRules.setVariable("rpm", this.calculateRpm());
+        fuzzyRules.setVariable("tilt", this.roadTilt);
 
         //logika sterownika
         fuzzyRules.evaluate();
@@ -53,15 +50,50 @@ public class Car {
         return speed * ratio[this.gear-1];
     }
 
-    private double calculateAcceleration() {
+    public double calculateTiltResistance(){
+        return 36.0/updatesPerSec * Math.sin(Math.toRadians(this.roadTilt));
+    }
+
+    public double calculateWindResistance(){
+        return speed*speed/(updatesPerSec*3600);
+    }
+
+    public double calculateAcceleration() {
+        if(brake) return -0.5;
+
+        double accel;
         double rpm = calculateRpm();
-        if(rpm < 6000){
-            double accel = rpm/12000 - speed*speed/180000;
-            return (throttle/100) * Math.max(accel, 0.01);
-        }
-        else {
-            double accel = 0.5 - ((rpm-6000)/12000)*((rpm-6000)/12000) - speed*speed/180000;
-            return (throttle/100) * Math.max(accel, 0);
-        }
+        if(rpm < 6000)
+            accel = rpm/12000 - calculateWindResistance();
+        else
+            accel = 0.5 - ((rpm-6000)/12000)*((rpm-6000)/12000) - calculateWindResistance();
+
+        return (0.5 + throttle/200) * Math.max(accel, 0.2);
+    }
+
+    private double calculateSpeedUpdate(){
+        return calculateAcceleration() - calculateTiltResistance();
+    }
+
+    public void setThrottle(double throttle) {
+        if(throttle >= 0 && throttle <= 100)
+            this.throttle = throttle;
+    }
+
+    public double getThrottle() {
+        return this.throttle;
+    }
+
+    public void setBrake(boolean b){
+        this.brake = b;
+    }
+
+    public void setTilt(double tilt) {
+        if(tilt >= -15 && tilt <= 15)
+            this.roadTilt = tilt;
+    }
+
+    public double getTilt() {
+        return this.roadTilt;
     }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,63 +5,123 @@ import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.geometry.Insets;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 import net.sourceforge.jFuzzyLogic.FIS;
 import net.sourceforge.jFuzzyLogic.rule.FuzzyRuleSet;
 
 public class Main extends Application {
+    public static int frameDelay = 20;
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) {
         String fileName = "fuzzy_transmission.fcl";
         FIS fis = FIS.load(fileName,false);
 
-        //wyswietl wykresy funkcji fuzyfikacji i defuzyfikacji
         FuzzyRuleSet fuzzyRuleSet = fis.getFuzzyRuleSet();
+//        fuzzyRuleSet.chart();
+        double[] speeds = {50, 50, 90, 90};
+        double[] throttles = {25, 100, 60, 60};
+        double[] tilts = {0, 0, -10, 10};
+        for(int i = 0; i < speeds.length; i++){
+            fuzzyRuleSet.setVariable("speed", speeds[i]);
+            fuzzyRuleSet.setVariable("throttle", throttles[i]);
+            fuzzyRuleSet.setVariable("tilt", tilts[i]);
+            fuzzyRuleSet.evaluate();
+            fuzzyRuleSet
+                    .getVariable("gear")
+                    .chartDefuzzifier(true)
+                    .setTitle(
+                            "Speed: " +
+                            speeds[i] +
+                            " Throttle: " +
+                            throttles[i] +
+                            " Tilt: " +
+                            tilts[i]
+                    );
+        }
+
         Car c = new Car(fuzzyRuleSet);
-        c.gearTest();
 
         GridPane mainGrid = new GridPane();
         mainGrid.setPadding(new Insets(20));
         mainGrid.setHgap(10);
         mainGrid.setVgap(10);
 
-        primaryStage.setTitle("Snake Main Menu");
         primaryStage.setResizable(true);
-        primaryStage.setMinHeight(600);
-        primaryStage.setMinWidth(1200);
+        primaryStage.setMinHeight(350);
+        primaryStage.setMinWidth(900);
         Scene scene = new Scene(mainGrid);
         primaryStage.setScene(scene);
 
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Fuzzy automatic trasmission simulation");
         primaryStage.show();
-//        Label speedL = new Label("0");
-//        Label rpmL = new Label("0");
-//        Label gearL = new Label("0");
 
-//        mainGrid.add(speedL, 0, 0);
-//        mainGrid.add(rpmL, 0, 1);
-//        mainGrid.add(gearL, 0, 2);
-//
-//        mainGrid.add(new Label("KM/H"), 1, 0);
-//        mainGrid.add(new Label("RPM"), 1, 1);
-//        mainGrid.add(new Label("Gear"), 1, 2);
-
-        Canvas canvas = new Canvas(1200, 800);
+        Canvas canvas = new Canvas(1000, 900);
         GraphicsContext gc = canvas.getGraphicsContext2D();
-        mainGrid.add(canvas, 0, 0);
+        mainGrid.add(canvas, 0, 1);
 
-        Timeline timeline = new Timeline(new KeyFrame(Duration.millis(20), event -> {
+        HBox controlBox = new HBox();
+        controlBox.setSpacing(50);
+        mainGrid.add(controlBox, 0, 0);
+
+        // Throttle control
+        Label throttleLabel = new Label("Throttle:");
+        controlBox.getChildren().add(throttleLabel);
+        Slider throttleSlider = new Slider();
+        throttleSlider.setMin(0);
+        throttleSlider.setMax(100);
+        throttleSlider.setMajorTickUnit(10);
+        throttleSlider.setMinorTickCount(10);
+        throttleSlider.setShowTickLabels(true);
+        throttleSlider.setValue(c.getThrottle());
+        throttleSlider.setVisible(true);
+        throttleSlider.valueProperty().addListener(
+                (o, oldValue, newValue) -> c.setThrottle(newValue.floatValue())
+        );
+        controlBox.getChildren().add(throttleSlider);
+
+        // Tilt control
+        Label tiltLabel = new Label("Tilt:");
+        controlBox.getChildren().add(tiltLabel);
+        Slider tiltSlider = new Slider();
+        tiltSlider.setMin(-15);
+        tiltSlider.setMax(15);
+        tiltSlider.setMajorTickUnit(5);
+        tiltSlider.setMinorTickCount(5);
+        tiltSlider.setShowTickLabels(true);
+        tiltSlider.setValue(c.getTilt());
+        tiltSlider.setVisible(true);
+        tiltSlider.valueProperty().addListener(
+                (o, oldValue, newValue) -> c.setTilt(newValue.floatValue())
+        );
+        controlBox.getChildren().add(tiltSlider);
+
+        Button brakeButton = new Button("Brake");
+        controlBox.getChildren().add(brakeButton);
+
+        Rectangle2D bounds = Screen.getPrimary().getVisualBounds();
+        double x = bounds.getMinX() + (bounds.getWidth() - scene.getWidth()) * 0.3;
+        double y = bounds.getMinY() + (bounds.getHeight() - scene.getHeight()) * 0.2;
+        primaryStage.setX(x);
+        primaryStage.setY(y);
+
+        Timeline timeline = new Timeline(new KeyFrame(Duration.millis(frameDelay), event -> {
+            c.setBrake(brakeButton.pressedProperty().get());
             c.update();
             gc.setFill(Color.WHITE);
-            gc.fillRect(0, 0, 800, 600);
+            gc.fillRect(0, 0, 850, 250);
             gc.setFill(Color.BLACK);
             for(int i = 0; i < 10; i++) {
                 gc.strokeText(
@@ -94,12 +154,10 @@ public class Main extends Application {
                     600 + 160*Math.cos(Math.PI + Math.PI*c.speed/300),
                     200 + 160*Math.sin(Math.PI + Math.PI*c.speed/300)
             );
-//            speedL.setText(String.valueOf((int)c.speed));
-//            rpmL.setText(String.valueOf((int)c.calculateRpm()));
-//            gearL.setText(String.valueOf(c.gear));
         }
         ));
         timeline.setCycleCount(Animation.INDEFINITE);
+        primaryStage.show();
         timeline.play();
 
     }
